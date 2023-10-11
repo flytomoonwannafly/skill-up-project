e3199e5e0d622e955dcd803d8c1c8d778b26b46e
vovaivanitskyi <vovaivanitskyi@gmail.com>
2023-10-11 13:36:57 +0300

    output a message if the required field is empty

--- application/views/form_note_view.php
+++ application/views/form_note_view.php
@@ -9,14 +9,36 @@ if (empty($data)) {
     $content = $data['content'];
 }
 ?>
-<form action="<?php echo $data_action; ?>" method="POST">
+<style>
+    #errorMessages {
+        color: red;
+    }
+</style>
+<form id="noteForm" action="<?php echo $data_action; ?>" method="POST">
     <div class="mb-3">
         <label for="title" class="form-label">Заголовок</label>
-        <input type="text" class="form-control" name="title" value="<?php echo $title ?>">
+        <input type="text" id="title" class="form-control" name="title" value="<?php echo $title ?>">
     </div>
     <div class="mb-3">
         <label for="content" class="form-label">Контент</label>
-        <textarea class="form-control" name="content" rows="5"><?php echo $content ?></textarea>
+        <textarea class="form-control" id="content" name="content" rows="5"><?php echo $content ?></textarea>
     </div>
     <button type="submit" name="submit" class="btn btn-primary">Зберегти</button>
 </form>
+<div id="errorMessages"></div>
+
+
+<script>
+    document.getElementById('noteForm').addEventListener('submit', function(event) {
+        // Перевірка, чи заповнені поля перед відправкою форми
+        var titleField = document.getElementById('title');
+        var textField = document.getElementById('content');
+        var errorMessages = document.getElementById('errorMessages');
+        errorMessages.innerHTML = '';
+
+        if (titleField.value.trim() === '' || textField.value.trim() === '') {
+            errorMessages.innerHTML = 'Заповніть обов\'язкові поля.';
+            event.preventDefault(); // Зупинка відправки форми
+        }
+    });
+</script>
